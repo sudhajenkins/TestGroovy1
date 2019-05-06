059ab52aec6ac997363c06807cb40b9133f3e64e
Devops_CG <sudhaajan14@gmail.com>
2019-05-06 21:21:32 +0530

    clean

--- vars/testing.groovy
+++ vars/testing.groovy
@@ -1,6 +1,7 @@
 #!/usr/bin/env groovy
 
-def call(args) {
-  echo "Welcome, ${name}."
-  steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
+def packageArtifact(){
+    stage("Package artifact") {
+        bat "mvn clean"
+    }
 }
