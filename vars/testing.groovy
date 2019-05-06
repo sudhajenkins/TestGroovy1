8dcd4829e26d8e1831363c632a89c9f22522bcc0
Devops_CG <sudhaajan14@gmail.com>
2019-05-06 22:52:20 +0530

    add new method

--- vars/testing.groovy
+++ vars/testing.groovy
@@ -1,9 +1,12 @@
 #!/usr/bin/env groovy
 
-def call() {
-    stage("Package artifact") {
-        bat "mvn clean"
+def call(body) {
+    echo "Check status"
+
+    (1..3).each {
+        echo "Number: " + it
     }
+
+    currentBuild.result = 'SUCCESS' //FAILURE to fail
+    return this
 }
-
-
